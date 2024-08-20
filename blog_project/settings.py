3c78f64a5d1e3b248147dcd1d91256a4d35b4dea
NerdyXG <MichaelOluwayemi11@gmail.com>
2024-08-20 22:35:10 +0100

    Create forms and user accounts

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
 
     # my apps
     'blog.apps.BlogConfig',
+    'accounts.apps.AccountsConfig',
 ]
 
 MIDDLEWARE = [
@@ -128,3 +129,6 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = '/'
